8f1c21509e2bc5afac3f3b907fb99c3cd0a0f9d2
Visioli-emanuele <visioli.emanuele@iisgalvanimi.edu.it>
2022-05-09 11:51:20 +0000

    metodi presi

--- MondoDB-Flask1/app.py
+++ MondoDB-Flask1/app.py
@@ -1,58 +1,124 @@
-from flask import Flask , request, jsonify, Response
-from flask_restful import Resource , Api ,reqparse
-from flask_cors import CORS
-from bson import json_util
-import pandas as pd
-
+from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from flask_cors import CORS
 
-app = Flask(__name__ )
 
-app.config["MONGO_URI"] = "mongodb://Emanuele-Visioli:EMAnuele25102004@cluster0-shard-00-00.x7fma.mongodb.net:27017,cluster0-shard-00-01.x7fma.mongodb.net:27017,cluster0-shard-00-02.x7fma.mongodb.net:27017/Sanita?ssl=true&replicaSet=atlas-566dc8-shard-0&authSource=admin&retryWrites=true&w=majority" #bisogna anche specificare il nome del database al interno del link 
-
+app = Flask(__name__)
+app.config["MONGO_URI"] = 'mongodb://Livera2003:Waduge78&@cluster0-shard-00-00.uylmr.mongodb.net:27017,cluster0-shard-00-01.uylmr.mongodb.net:27017,cluster0-shard-00-02.uylmr.mongodb.net:27017/Livera1?ssl=true&replicaSet=atlas-38dmxu-shard-0&authSource=admin&retryWrites=true&w=majority'
+# db = client.lin_flask
 mongo = PyMongo(app)
-
 CORS(app)
-api = Api(app)
 
+@app.route('/')
+def index():
+    return "Ciao00000"
 
-#-------------------------------------------------------------------------------------------------------------------Prova
-
-class UsersApi(Resource):
-    #def get(self):
-    #    uss = mongo.db.medici_medicina_generale.find()
-    #    resp = json_util.dumps(uss)
-    #    return Response(resp, mimetype = 'application/json') 
-    def post(self):
-        user = request.json["user"]
-        informatica = request.json["informatica"]
-        matematica = request.json["matematica"]
-        arte = request.json["arte"]
-        if user and informatica and matematica and arte:
-            id = mongo.db.Prova1.insert_one(
-                {
-                'user': user,
-                'informatica': informatica,
-                'matematica': matematica,
-                'arte': arte 
-                }
-            )
-            resp = {
-                "id" : str(id),
-                'user': user,
-                'informatica': informatica,
-                'matematica': matematica,
-                'arte': arte 
+@app.route('/users', methods=['POST', 'GET'])
+def data():
+    
+    # POST a data to database
+    if request.method == 'POST':
+        body = request.json
+        name = body['name']
+        emailId = body['emailId']
+        phone = body['phone']
+        date = body['date']
+        reason = body['reason']
+        # db.users.insert_one({
+        mongo.db.users.insert_one({
+            "name": name,
+            "emailId":emailId,
+            "phone":phone,
+            'date':date,
+            'reason':reason
+        })
+        return jsonify({
+            'status': 'Data is posted to MongoDB!',
+            'name': name,
+            'emailId':emailId,
+            'phone':phone,
+            'date':date,
+            'reason':reason
+        })
+    
+    # GET all data from database
+    if request.method == 'GET':
+        allData =  mongo.db.users.find()
+        dataJson = []
+        for data in allData:
+            id = data['_id']
+            name = data['name']
+            emailId = data['emailId']
+            phone = data['phone']
+            date = data['date']
+            reason = data['reason']
+            dataDict = {
+                'id': str(id),
+                'name': name,
+                'emailId': emailId,
+                'phone':phone,
+                'date':date,
+                'reason':reason
             }
-            return resp
-        else:
-            return {'message': 'received'}
-    def delete(self):
-        #resp = mongo.db.prova.delete_one( { "_id": ObjectId("6274ed1cc8f6c95a70e9902e") } )
-        return Response("ciao")#resp.deleted_count, mimetype = 'application/json') 
-api.add_resource(UsersApi, '/users')
+            dataJson.append(dataDict)
+        print(dataJson)
+        return jsonify(dataJson)
 
+@app.route('/users/<string:id>', methods=['GET', 'DELETE', 'PUT'])
+def onedata(id):
 
+    # GET a specific data by id
+    if request.method == 'GET':
+        data = mongo.db.users.find_one({'_id': ObjectId(id)})
+        id = data['_id']
+        name = data['name']
+        emailId = data['emailId']
+        phone = data['phone']
+        date = data['date']
+        reason = data['reason']
+        dataDict = {
+            'id': str(id),
+            'name': name,
+            'emailId':emailId,
+            'phone':phone,
+            'date':date,
+            'reason':reason
+        }
+        print(dataDict)
+        return jsonify(dataDict)
+        
+    # DELETE a data
+    if request.method == 'DELETE':
+        mongo.db.appuntamenti.delete_many({'_id': ObjectId(id)})
+        print('\n # Deletion successful # \n')
+        return jsonify({'status': 'Data id: ' + id + ' is deleted!'})
+
+    # UPDATE a data by id
+    if request.method == 'PUT':
+        body = request.json
+        name = body['name']
+        emailId = body['emailId']
+        phone = body['phone']
+        date = body['date']
+        reason = body['reason']
+
+        mongo.db.users.update_one(
+            {'_id': ObjectId(id)},
+            {
+                "$set": {
+                    "name":name,
+                    "emailId": emailId,
+                    "phone":phone,
+                    "date":date,
+                    "reason":reason
+                }
+            }
+        )
+
+        print('\n # Update successful # \n')
+        return jsonify({'status': 'Data id: ' + id + ' is updated!'})
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
